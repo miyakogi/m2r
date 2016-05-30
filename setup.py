2dcf97ab8d8849ac9cbbf8a723889200559d852e
miyakogi <miyako.dev@gmail.com>
2016-05-31 01:17:35 +0900

    Update meta data

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ install_requires = ['mistune', 'docutils']
 setup(
     name='m2r',
     version='0.1.0',
-    description='Markdown to restructuredtext converter',
+    description='Markdown to reStructuredText converter',
     long_description=readme,
     author='Hiroyuki Takagi',
     author_email='miyako.dev@gmail.com',
