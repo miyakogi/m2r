e4e38f7b8eedf4f1a55364aa2fdd0c06f23972cb
miyakogi <miyako.dev@gmail.com>
2016-06-09 04:34:45 +0900

    Bump version: 0.1.3 → 0.1.4

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ install_requires = ['mistune', 'docutils']
 
 setup(
     name='m2r',
-    version='0.1.3',
+    version='0.1.4',
     description='Markdown to reStructuredText converter',
     long_description=readme,
     author='Hiroyuki Takagi',
