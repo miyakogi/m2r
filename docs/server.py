185256911b82adce178ef4afa4714877206d83e0
miyakogi <miyako.dev@gmail.com>
2017-08-11 16:50:16 +0900

    Update document preview server

--- docs/server.py
+++ docs/server.py
@@ -3,23 +3,37 @@
 
 from os import path
 import subprocess
-from livereload import Server, shell
+
+from livereload import Server
 
 docsdir = path.dirname(path.abspath(__file__))
 builddir = path.join(docsdir, '_build')
+build_cmd = [
+    'sphinx-build', '-b', 'html', '-E', '-q',
+    '-d', path.join(builddir, 'doctrees'),
+    docsdir, path.join(builddir, 'html'),
+]
 
-subprocess.run(['make', 'clean'])
-subprocess.run(['make', 'html'])
-cmd = shell(['sphinx-build', '-b', 'html', '-E',
-             '-d', path.join(builddir, 'doctrees'),
-             docsdir, path.join(builddir, 'html')])
 
+def cmd() -> None:
+    print('=== Sphinx Build Start ===')
+    subprocess.run(build_cmd, cwd=docsdir)
+    print('=== Sphinx Build done ===')
+
+
+def docs(p: str) -> str:
+    return path.join(docsdir, p)
+
+
+# subprocess.run(['make', 'clean'], cwd=docsdir)
+cmd()  # build once
 server = Server()
-server.watch('*.py', cmd)
-server.watch('../*.py', cmd)
-server.watch('*.md', cmd)
-server.watch('../*.md', cmd)
-server.watch('_static/*.css', cmd)
-server.watch('_templates/*.html', cmd)
 
-server.serve(port=8889, root='_build/html', debug=True, restart_delay=1)
+server.watch(docs('*.py'), cmd)
+server.watch(docs('../*.py'), cmd)
+server.watch(docs('*.md'), cmd)
+server.watch(docs('../*.md'), cmd)
+server.watch(docs('_static/*.css'), cmd)
+server.watch(docs('_templates/*.html'), cmd)
+
+server.serve(port=8889, root=docs('_build/html'), debug=True, restart_delay=1)
